1c0af10e49dde6bddfb294d8589cee3a97c1b722
Cian Rodriguez <crod0007@student.monash.edu>
2023-08-22 15:04:56 +1000

    latestupdates to ekf

--- Week03-05/slam/ekf.py
+++ Week03-05/slam/ekf.py
@@ -91,8 +91,9 @@ class EKF:
         x = self.get_state_vector()
 
         # TODO: add your codes here to compute the predicted x
+        self.robot.drive(raw_drive_meas)
         self.P = F @ self.P @ np.transpose(F) + self.predict_covariance(raw_drive_meas)
-        updated_x = F @ x
+        # updated_x = F @ x
         # self.set_state_vector(updated_x)
         # print(f"Old x:     {np.round(x.T,2)}")
         # print(f"Updated x: {np.round(updated_x.T,2)}")
