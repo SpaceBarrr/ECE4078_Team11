d1e5a47201495cb0b4f07c50d70e81242e493510
Cian Rodriguez <crod0007@student.monash.edu>
2023-09-12 14:08:55 +1000

    finish implementing their bugfix

--- Week06-07/TargetPoseEst.py
+++ Week06-07/TargetPoseEst.py
@@ -38,7 +38,7 @@ def estimate_pose(camera_matrix, obj_info, robot_pose):
     # TODO: measure actual sizes of targets [width, depth, height] and update the dictionary of true target dimensions
     target_dimensions_dict = {'Orange': [0.077,0.078,0.074], 'Lemon': [0.070,0.051,0.053], # SWAP BACK FOR FAKE FRUIT 'Orange': [0.077,0.078,0.074], 'Lemon': [0.070,0.051,0.053], REAL Fruit : 'Orange': [0.0963,0.0892,0.0871], 'Lemon': [0.0651,0.091,0.0642]
                               'Lime': [0.073,0.053,0.051], 'Tomato': [0.072,0.073,0.062], 
-                              'Capsicum': [0.079,0.076,0.097], 'Potato': [0.095,0.060,0.067], 
+                              'Capsicum': [0.079,0.076,0.097], 'potato': [0.095,0.060,0.067], 
                               'Pumpkin': [0.087,0.085,0.075],  'Garlic': [0.064,0.061,0.073]} # SWAP BACK FOR FAKE FRUIT 'Garlic': [0.064,0.061,0.073], real fruit : 'Garlic': [0.057,0.056,0.052]
     #########
     # estimate target pose using bounding box and robot pose
