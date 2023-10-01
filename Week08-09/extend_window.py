c842a39ae733e8a1bf4d9a1a0f13050bb0948528
JOEL REYNOLDS <jrey0009@student.monash.edu>
2023-10-01 14:38:29 +1100

    extend_window fix

--- Week08-09/extend_window.py
+++ Week08-09/extend_window.py
@@ -16,7 +16,7 @@ pygame.draw.rect(screen,rect_colour,map_background_rect)
 
 
 
-second_image = pygame.image.load('Week08-09/map_image.png')
+second_image = pygame.image.load('map_image.png')
 second_image = pygame.transform.scale(second_image, (400, 400))
 screen.blit(second_image, (700, 0))
 origin_dot = pygame.Rect(904,201,4,4)
